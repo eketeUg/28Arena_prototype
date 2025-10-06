5a0c3ea0a1fbdeba7e4cb7c6ceff33fe5d80029d
eketeUg <emmanuelugochukwuekete@gmail.com>
2025-10-06 13:08:21 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ export default function App() {
         overflow: "hidden",
       }}
     >
-      <h1 style={{ marginBottom: "10px" }}>⚔️ Arena 28</h1>
+      <h1 style={{ marginBottom: "10px" }}>⚔️ Arena 28 test</h1>
       <div
         id="game-wrapper"
         style={{
